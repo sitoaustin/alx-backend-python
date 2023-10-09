c07f9a6c07ee800fdfb605d8425f92d38909bcd8
“sitoaustin” <onyedibesixtusna@gmail.com>
2023-10-09 15:35:43 +0100

    Fixed the import issue

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-import wait_random
 import asyncio
 from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
