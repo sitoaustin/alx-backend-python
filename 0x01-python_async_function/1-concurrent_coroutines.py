c02a81c889b2b93e8b18331b63845756dd2317b4
“sitoaustin” <onyedibesixtusna@gmail.com>
2023-10-09 14:03:33 +0100

    Working with random in python

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''Write an asynchronous coroutine that takes
+in an integer argument (max_delay, with a default
+value of 10) named wait_random that waits for
+ a random delay between 0 and max_delay (included and float value)
+seconds and eventually returns it.
+'''
+import random
+
+
+def wait_random(max_delay=10):
+    randNum = random.randrange(0, max_delay)
+    return randNum
